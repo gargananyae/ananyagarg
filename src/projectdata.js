552d777187205f56c9e5a9db34a27d40cc37d0c3
Ananya Garg <gargananya2@gmail.com>
2023-06-27 00:14:49 +0530

    Figmafiles

--- src/projectdata.js
+++ src/projectdata.js
@@ -6,7 +6,7 @@ const projectList = [
       "An unique solution that would help to include informal vendors in the public space development without compromising on the Urban Design.  ",
     image: require("./images/Vendos.png"),
     github: "https://github.com/gargananyae/SIH-vendo",
-    tools: ["Next.js", "Dart", "Flutter", "MongoDB"]
+    tools: ["Next.js", "Dart", "Flutter", "MongoDB"],
   },
   {
     id: 1,
@@ -21,7 +21,8 @@ const projectList = [
   {
     id: 2,
     name: "Paws Hunter",
-    description: "A website made for an NGO for helping and adopting injured and helpless animals ",
+    description:
+      "A website made for an NGO for helping and adopting injured and helpless animals ",
     image: require("./images/paws.png"),
     github: "https://github.com/Gaurav29502/need-for-code-11/tree/master",
     tools: ["React.js", "google maps API", "Mongo DB"],
@@ -54,13 +55,47 @@ const projectList = [
   {
     id: 5,
     name: "Regression Analysis",
-    description:"Reaction rate constant prediction using regression analysis",
+    description: "Reaction rate constant prediction using regression analysis",
     image: require("./images/multireg.png"),
     github: "https://github.com/gargananyae/MultiReg",
     tools: ["Python", "Excel"],
     inView: false,
   },
-  
+
+  {
+    id: 6,
+    name: " Figma- Food Hunt",
+    description:
+      "A figma project (dashboard) made to experiment with masking and animation functionalities of Figma.",
+    image: require("./images/FoodHunt.png"),
+    github:
+      "https://www.figma.com/proto/rbltYpSBOCKzZEdAiSc6Vt/Food-Hunt?node-id=1-2&starting-point-node-id=1%3A2",
+    tools: ["Figma"],
+    inView: false,
+  },
+
+  {
+    id: 7,
+    name: "Judo Class Web Design",
+    description: "An initial level webpage design for my sister's Judo class.",
+    image: require("./images/judo.png"),
+    github:
+      "https://www.figma.com/file/mp19dKRIV4PU4v9257Ir7K/Judo-tutorial-website?type=design&node-id=0-1&mode=design&t=2pbQ7uYec1SqD5rg-0",
+    tools: ["Figma"],
+    inView: false,
+  },
+
+  {
+    id: 8,
+    name: "Clueless- A fashion app",
+    description:
+      "UI for a fashion application currently being made for final year project.",
+    image: require("./images/clueless.png"),
+    github:
+      "https://www.figma.com/file/eaFNL2dUAHqurkpjSJnhpm/Clueless?type=design&mode=design&t=2pbQ7uYec1SqD5rg-0",
+    tools: ["Figma"],
+    inView: false,
+  },
 ];
 
 export default projectList;
